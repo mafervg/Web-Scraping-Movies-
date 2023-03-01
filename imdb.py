acd018ab38065961cdfba4ced4daebe2365787fb
mafervg <mariafvg15@outlook.com>
2023-02-28 22:19:04 -0500

    Updated code for Imdb database

--- imdb.py
+++ imdb.py
@@ -5,10 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
-import re
 import requests
 from bs4 import BeautifulSoup
-import json
 
 chrome_driver_path = "C:\Development\chromedriver.exe"
 service = Service(chrome_driver_path)
@@ -23,7 +21,6 @@ HEADERS = {
 
 df = pd.read_csv('50 best films of 2022.csv')
 titles = df['movie_title'].values.tolist()
-# titles = ['Jackass Forever','Everything Everywhere All at Once', 'Women Talking']
 
 movies_info = []
 
@@ -31,48 +28,63 @@ driver.get(URL)
 driver.maximize_window()
 time.sleep(1)
 for title in titles[::-1]:
-
     IMDb_search = driver.find_element(By.NAME, 'q')
     IMDb_search.send_keys(title)
     button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'suggestion-search-button')))
     button.click()
     time.sleep(1)
-    container = driver.find_element(By.CLASS_NAME, 'ipc-page-section')
+
+    # Wait until the container is loaded
+    # container = driver.find_element(By.CLASS_NAME, 'ipc-page-section')
+    container = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'ipc-page-section')))
     first_result = container.find_element(By.XPATH, '//*[@id="__next"]/main/div[2]/div[3]/section/div/div[1]/section[2]/div[2]/ul/li[1]/div[2]/div/a')
     driver.execute_script("arguments[0].click();", first_result)
     time.sleep(1)
+
+    # Wait until the page is loaded
     driver.switch_to.window(driver.window_handles[0])
     current_url = driver.current_url
     time.sleep(1)
 
+    # Wait until the page content is loaded
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, 'h1')))
+
     response = requests.get(current_url, headers=HEADERS)
     imdb_page = response.content
-
     soup = BeautifulSoup(imdb_page, 'html.parser')
 
     # Find Movie Title
     movie_title = soup.find('h1').text.strip()
 
     # Find Year
-    general_info = soup.find('div', {'class': 'sc-b5e8e7ce-2'})
-    general_container = general_info.find_next('ul')
-    general_tags = general_container.find_all('a')
-    year_rate = [g.text.strip() for g in general_tags]
+    # general_info = soup.find('div', {'class': 'sc-eda143c4-0'})
+    # general_container = general_info.find_next('ul')
+    # general_tags = general_container.find_all('a')
+    # year_rate = [g.text.strip() for g in general_tags]
 
     #  Find Genres
     genres_container = soup.find('div', {'class': 'ipc-chip-list__scroller'})
     genres = [genre.text.strip() for genre in genres_container.findAll('span', {'class': 'ipc-chip__text'})]
 
     # Find Director
-    director_tag = soup.find('div', class_='sc-b13e9d78-0')
+    director_tag = soup.find('div', class_='ipc-metadata-list-item__content-container')
     director_link = director_tag.find('a')['href']
     director_name = director_tag.find('a').text.strip()
 
     # Find the Writers
-    writers_tag = soup.find('li', class_='ipc-metadata-list__item')
-    writers_container = writers_tag.find_next('ul')
-    writers = writers_container.find_all('a')
-    writers_names = [w.text.strip() for w in writers]
+    try:
+        writers_tag = soup.find('a', text='Writers')
+        writers_container = writers_tag.find_next('ul')
+        writers = writers_container.find_all('a')
+        writers_names = [w.text.strip() for w in writers]
+    except:
+        writers_tag = soup.find('button', text=['Writer', 'Writers'])
+        if writers_tag:
+            writers_container = writers_tag.find_next('ul')
+            writers = writers_container.find_all('a')
+            writers_names = [w.text.strip() for w in writers]
+        else:
+            writers_names = ''
 
     # Find the Stars
     try:
@@ -87,7 +99,7 @@ for title in titles[::-1]:
     metascore = soup.find('span', {'class': 'score-meta'}).text.strip()
 
     # Find the imdb rating
-    imdb_rating = soup.find('span', {'class': 'sc-e457ee34-1 gvYTvP'}).text.strip()
+    imdb_rating = soup.find('span', {'class': 'sc-e457ee34-1'}).text.strip()
 
     # Go to director's page
     driver.get(f"https://www.imdb.com{director_link}")
@@ -98,8 +110,11 @@ for title in titles[::-1]:
     director_soup = BeautifulSoup(director_page, 'html.parser')
 
     # Get Image_URL
-    director_image_element = director_soup.find('img', {'class': 'ipc-image'})
-    director_image_url = director_image_element['src']
+    try:
+        director_image_element = director_soup.find('img', {'class': 'ipc-image'})
+        director_image_url = director_image_element['src']
+    except:
+        director_image_url = 'NaN'
 
     # Get Birthday and Country
     try:
@@ -109,48 +124,42 @@ for title in titles[::-1]:
         birthplace = born_element.find_all('a')[2].text.strip()
 
     except:
-        birthdate = ''
-        birthyear = ''
-        birthplace = ''
+        born_element = director_soup.find('li', {'data-testid': 'nm_pd_bl'})
+        birthdate = 'NaN'
+        birthyear = 'NaN'
+        birthplace = 'NaN'
 
-    try:
-        movie_dict = {
-            'movie_title': movie_title,
-            'year': year_rate[0],
-            'rate': year_rate[1],
-            'director': {
-                'name': director_name,
-                'birthdate': birthdate,
-                'birthyear': birthyear,
-                'birthplace': birthplace
-            },
-            'writers': writers_names,
-            'stars': stars_names,
-            'metascore': metascore,
-            'imdb_rating': imdb_rating
-        }
-    except IndexError:
-        movie_dict = {
-            'movie_title': movie_title,
-            'year': 'NA',
-            'rate': 'NA',
-            'director': {
-                'name': director_name,
-                'birthdate': birthdate,
-                'birthyear': birthyear,
-                'birthplace':  birthplace
-            },
-            'writers': writers_names,
-            'stars': stars_names,
-            'metascore': metascore,
-            'imdb_rating': imdb_rating
-        }
+        if born_element:
+            born_links = born_element.find_all('a')
+            if len(born_links) >= 1:
+                birthyear = born_links[0].text.strip()
+            if len(born_links) >= 2:
+                birthplace = born_links[1].text.strip()
+            if len(born_links) >= 3:
+                birthdate = born_links[2].text.strip()
+
+    movie_dict = {
+        'movie_title': movie_title,
+        # 'year': year_rate[0] if len(year_rate) > 0 else 'NaN',
+        # 'rate': year_rate[1] if len(year_rate) > 1 else 'NaN',
+        'genres': genres,
+        'director_name': director_name,
+        'director_birthdate': birthdate,
+        'director_birthyear': birthyear,
+        'director_birthplace': birthplace,
+        'director_img_url': director_image_url,
+        'writers': writers_names,
+        'stars': stars_names,
+        'metascore': metascore,
+        'imdb_rating': imdb_rating
+    }
 
     # append the dictionary to the list of movies
     movies_info.append(movie_dict)
+    print(movie_dict)
 
 # print(movies_info)
 
 df1 = pd.DataFrame(movies_info)
-df1.to_csv('imdb_info.csv')
+df1.to_csv('new_imdb_info.csv')
 print(df1)
